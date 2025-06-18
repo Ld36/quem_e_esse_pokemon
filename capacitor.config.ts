6038cd832a842cde1cc7242071bfd84a5c98b7f8
Ld36 <luizdavidx798@gmail.com>
2025-06-18 09:26:44 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'quem_e_esse_pokemon_novo',
+  webDir: 'www'
+};
+
+export default config;
